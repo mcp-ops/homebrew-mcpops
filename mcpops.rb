6792c03a7e17cb623e4f12473a70daa542e3c4e9
Nguyen Thanh Dong <dongnt@192.168.1.68>
2025-08-01 00:26:25 +0700

    fix error install

--- mcpops.rb
+++ mcpops.rb
@@ -1,11 +1,9 @@
-class Mcpops < Formula
+cask "mcpops" do
   desc "Cross-platform MCP configuration management for all applications"
   homepage "https://mcpops.io"
   url "https://github.com/mcp-ops/app-releases/releases/download/mac-v0.1.0/MCP.Operations.app.tar.gz"
   sha256 "ac2d3e5f87f5b238df60bed40b63599680cf328cbc5fc5513bf1e6392514cedc"
   version "0.1.0"
 
-  def install
-    app.install "MCP Operations.app"
-  end
+  app "MCP Operations.app"
 end
